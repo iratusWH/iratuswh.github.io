671ecd0e505906a768b3b75d923dc20367a9a3df
iratusWH <68867409+iratusWH@users.noreply.github.com>
2021-06-22 14:38:34 +0300

    store beta-ver release

---
+++ store/scr.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.header_burger').click(function(event){
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+});
+
